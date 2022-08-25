bc9812c00a538bf7493f0d98ed19e8643155debb
Hongrokgi <Hongrokgi@naver.com>
2022-08-25 10:59:26 +0900

    Git Repository 생성 후 연동

---
+++ src/main/java/com/schedular/schedualr/SchedualrApplication.java
@@ -0,0 +1,13 @@
+package com.schedular.schedualr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedualrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedualrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedular/schedualr/SchedualrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedular.schedualr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedualrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
